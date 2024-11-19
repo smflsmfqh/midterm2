0008ed7efa28a70cda1a38a4f645f28576f82954
haneullee <skyee46@gmail.com>
2024-11-19 13:36:17 +0900

    4

--- PhotoBlogServer/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Post, Image
-
-class PostAdmin(admin.ModelAdmin):
-    fields = ('author', 'title', 'text', 'created_date', 'published_date', 'image')  # 여기에 이미지 필드를 추가
-
-admin.site.register(Post, PostAdmin)
